6b9703807dfb1515ed8f0755ea0dafcdc65b64fc
Kan-prog <forwkan@gmail.com>
2021-05-10 11:26:55 +0900

    playgroundにてエラー発生中「error: Failed to fetch. Please check your connection」

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// Appoloの依存関係をimport
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+import * as serviceWorker from './serviceWorker';
+
+
+// GraphQLのhttpリンクを設定
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// クライアントとhttplinkを紐づけ
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
